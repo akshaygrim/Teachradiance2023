a6f558a264d077d90eec265898cea71e41ecbdef
Akshay <111304670+akshaygrim@users.noreply.github.com>
2024-05-17 01:23:49 -0700

    Update checker.py

--- checker.py
+++ checker.py
@@ -2,14 +2,14 @@ import numpy as np #importing numpy module NP
 # we will define a function for the checker
 def checker(n):
 	# crearting a variable x that will be a matrix
-	org = np.zeros((n, n), dtype=int)
+	gg = np.zeros((n,n),dtype=int)
 # adding the rows and coloumns 
-	org[1::2, ::2] =1
-	org[::2, 1::2] =1
+	gg[1::2, ::2] =1
+	gg[::2, 1::2] =1
 # printing the entire pattern
 	for i in range(n):
 		for j in range(n):
-			print(org[i][j], end=" ")
+			print(gg[i][j], end=" ")
 		print()
 # n value 
 n = 8
