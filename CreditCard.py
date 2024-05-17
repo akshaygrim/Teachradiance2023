65b164d1668a735082ab422bad09cb9265ed3429
Akshay <111304670+akshaygrim@users.noreply.github.com>
2024-05-17 01:22:34 -0700

    CreditCard.py

--- CreditCard.py
+++ CreditCard.py
@@ -1,10 +1,10 @@
-ccno = '1347-1067-4839-4562'
- #defieing the functions 
+cc = '1337-1767-4239-4862'
+ #defining the functions 
 def hideccno(ccno):
     strng = '' #empty string
-    if len(ccno) > 4:
-        strng += '*' *(len(ccno) - 4)+ccno[-4:] #here we are slicing the strings to just blur the first digits
+    if len(cc) > 4:
+        strng += '*' *(len(cc) - 4)+cc[-4:] #here we are slicing the strings to just blur the first digits
     else:
-        return ccno
+        return cc
     return strng
-print(hideccno(ccno))
+print(hideccno(cc))
